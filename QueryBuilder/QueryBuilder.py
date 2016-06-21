6435ff2d483fc5474bb6dd69c5b8f69c756b50f5
Alexander Freeman <alexander.freeman.nl@gmail.com>
2016-06-21 10:17:50 +0200

    Added first flask app setup

---
+++ QueryBuilder/QueryBuilder.py
@@ -0,0 +1,48 @@
+import flask
+from wit import Wit
+import logging
+
+app = flask.Flask(__name__)
+client = Wit(token, actions)
+
+@app.route("/")
+def index():
+    """
+    When you request the root path, you'll get the index.html template.
+
+    """
+    return flask.render_template("index.html")
+
+@app.route("/parse_and_run_query", methods=['GET', 'POST'])
+def query():
+    """
+    Parses the given sentence and returns the corresponding geodata
+
+    :returns data:
+        A JSON array containing the geodata when successfull. An empty array
+        when not successful.
+    """
+
+    if flask.request.method == 'POST':
+        json_data = flask.request.get_json(force=True)
+        print(json_data)
+        if not json_data["sentence"]:
+            return flask.jsonify({})
+        return flask.jsonify({'result': "Success"})
+
+
+    else:
+        return flask.render_template("query.html")
+
+
+if __name__ == "__main__":
+    import os
+
+    port = 8085
+
+    # Open a web browser pointing at the app.
+    os.system("open http://localhost:{0}".format(port))
+
+    # Set up the development server on port 8000.
+    app.debug = True
+    app.run('0.0.0.0', port=port)
