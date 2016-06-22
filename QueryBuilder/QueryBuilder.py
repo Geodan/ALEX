5925ff57a710198ef4d30a4217da729e2e791418
Alexander Freeman <alexander.freeman.nl@gmail.com>
2016-06-22 14:41:04 +0200

    Words are ordered into stacks of types

--- QueryBuilder/QueryBuilder.py
+++ QueryBuilder/QueryBuilder.py
@@ -1,8 +1,8 @@
 import flask
 from wit import Wit
 import logging
-
 import config
+import requests
 
 def say(session_id, context, msg):
     print(msg)
@@ -36,7 +36,7 @@ def query():
     Parses the given sentence and returns the corresponding geodata
 
     :returns data:
-        A JSON array containing the geodata when successfull. An empty array
+        A JSON array containing the geodata when successful. An empty array
         when not successful.
     """
 
@@ -44,11 +44,83 @@ def query():
         json_data = flask.request.get_json(force=True)
         if not json_data["sentence"]:
             return flask.jsonify({})
-        resp = client.converse('geobot-session-1', json_data["sentence"], {})
+        try:
+            resp = client.converse('geobot-session-1', json_data["sentence"], {})
+        except:
+            return flask.jsonify({'error':'Wit returned an error'})
+
         print(resp)
-        return flask.jsonify(resp)
+
+        if "command" not in resp["entities"]:
+            return flask.jsonify({}) #What do I have to do with the the result?
+
+        if "search_query" not in resp["entities"]:
+            return flask.jsonify({}) #What do I have to search?
+
+        if "filter" not in resp["entities"]:
+            return flask.jsonify({}) #How do I limit the results?
+
+        sentence = json_data["sentence"].lower().strip()
 
 
+        command = None
+        search = []
+        logic = []
+        conditionals = []
+        filter = []
+        arguments = []
+
+
+        top_confidence = 0
+        for c in resp["entities"]["command"]:
+            if c["confidence"] > top_confidence:
+                top_confidence = c["confidence"]
+                command = c["value"]
+
+        counter = 0
+        while len(sentence) > 0:
+
+            for type in resp["entities"]:
+                for word_info in resp["entities"][type]:
+                    word = str(word_info["value"]).lower()
+                    try:
+                        if sentence.index(word) == 0:
+                            sentence = sentence.replace(word, "", 1).strip()
+
+                            if type == "search_query":
+                                print("Search", word)
+                                search.append((counter, word))
+                            elif type == "filter":
+                                filter.append((counter, word))
+                            elif type == "distance":
+                                arguments.append((counter, (word, word_info["unit"])))
+                            elif type == "logic_operator":
+                                logic.append((counter, word))
+                            elif type == "location":
+                                arguments.append((counter, word))
+
+                            counter += 1
+                            if type == "distance":
+                                sentence = sentence[sentence.find(' '):].strip()
+                                if sentence.find(' ') < 0:
+                                    sentence = ''
+                                    break
+
+                    except ValueError as e:
+                        pass
+
+            print("Sentence left: ", sentence)
+        print("Done :)")
+
+        flask_response = {
+            'command': command,
+            'search': search,
+            'logic': logic,
+            'filter': filter,
+            'conditionals': conditionals,
+            'arguments': arguments
+        }
+        return flask.jsonify(flask_response)
 
     else:
         return flask.render_template("query.html")
