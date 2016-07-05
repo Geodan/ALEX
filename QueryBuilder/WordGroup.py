8cb9919bb0297c243276b26297445ee4c8306c28
Alexander Freeman <alexander.freeman.nl@gmail.com>
2016-07-05 10:38:20 +0200

    Added a lot of helper methods to WordGroup.py

--- QueryBuilder/WordGroup.py
+++ QueryBuilder/WordGroup.py
@@ -1,8 +1,29 @@
 class WordGroup:
 
     def __init__(self, sentence):
-        self.words = self.split_words(sentence)
-        self.part = sentence
+        self.words = self.split_words(sentence.lower())
+        self.sentence = sentence.lower()
+
+    # Copied from http://stackoverflow.com/questions/17870544/find-starting-and-ending-indices-of-sublist-in-list
+    # Modified to work with my own codebase
+    def find_in_word_group(self, wg):
+        sl = self.words
+        l = wg.words
+
+        sll=len(sl)
+        for ind in (i for i,e in enumerate(l) if e==sl[0]):
+            if l[ind:ind+sll]==sl:
+                return ind,ind+sll-1
+        return (-1, -1)
+
+    def remove_n_words_from_front(self, n):
+        if len(self.words) > n:
+            self.words = self.words[n:]
+            self.sentence = "".join(self.words)
+        else:
+            self.words = []
+            self.sentence = "".join(self.words)
+
 
     def trim_next_word_off_sentence(self, pos):
 
@@ -48,6 +69,22 @@ class WordGroup:
                 result.append(next_word)
             words = self.trim_next_word_off_sentence(words)
         return result
+
+    def sequelize(self, arguments):
+        """
+        Returns a list of tuples containing SQL queries and an int
+        representing the place where the subquery
+        must be placed in the the total query. For instance: a 1 (one)
+        will be placed as the 2nd subquery (zero indexed).
+        If the int is < 0, it must be placed relative to the wordgroups
+        index in the sentence.
+
+        Parameters:
+            self - The current WordGroup
+            arguments - The stack of arguments in the complete sentence
+        """
+
+        raise NotImplementedError
 
 if __name__ == "__main__":
     wg = WordGroup("This class creates a word group from a string")
