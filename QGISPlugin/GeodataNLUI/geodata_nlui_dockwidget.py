0a772a158e7ed8cc841dad9afb253256ee0c8a59
Alexander Freeman <alexander.freeman.nl@gmail.com>
2016-07-12 09:32:35 +0200

    Added request on button press

--- QGISPlugin/GeodataNLUI/geodata_nlui_dockwidget.py
+++ QGISPlugin/GeodataNLUI/geodata_nlui_dockwidget.py
@@ -22,8 +22,11 @@
 """
 
 import os
+import requests
+import json
 
 from PyQt4 import QtGui, uic
+from qgis.utils import iface
 from PyQt4.QtCore import pyqtSignal
 
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
@@ -34,6 +37,18 @@ class GeodataNLUIDockWidget(QtGui.QDockWidget, FORM_CLASS):
 
     closingPlugin = pyqtSignal()
 
+    def parse_and_run(self):
+        iface.messageBar().pushInfo(u'NLUI', u'button clicked')
+
+        url = 'http://localhost:8085/parse_and_run_query'
+        payload = json.dumps({'sentence':  self.nlquery.toPlainText()})
+        headers = {'content-type': 'application/json'}
+        self.parserunbutton.setEnabled(False)
+        response = requests.post(url, data=payload, headers=headers).content.decode("utf-8")
+        self.nlquery.setText(response)
+        self.parserunbutton.setEnabled(True)
+
+
     def __init__(self, parent=None):
         """Constructor."""
         super(GeodataNLUIDockWidget, self).__init__(parent)
@@ -43,8 +58,9 @@ class GeodataNLUIDockWidget(QtGui.QDockWidget, FORM_CLASS):
         # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
         # #widgets-and-dialogs-with-auto-connect
         self.setupUi(self)
+        self.parserunbutton.clicked.connect(self.parse_and_run)
+
 
     def closeEvent(self, event):
         self.closingPlugin.emit()
         event.accept()
-
