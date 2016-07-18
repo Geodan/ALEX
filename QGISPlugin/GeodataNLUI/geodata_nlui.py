fc39800ab12e9db41f33683f553de61b2608e2b6
Alexander Freeman <alexander.freeman.nl@gmail.com>
2016-07-18 14:26:35 +0200

    Removed license at the top of the file

--- QGISPlugin/GeodataNLUI/geodata_nlui.py
+++ QGISPlugin/GeodataNLUI/geodata_nlui.py
@@ -6,19 +6,10 @@
  This plugin provides a GUI for our NLUI
                               -------------------
         begin                : 2016-07-11
-        git sha              : $Format:%H$
         copyright            : (C) 2016 by Geodan (Alexander Freeman)
         email                : alexander.freeman@geodan.nl
  ***************************************************************************/
 
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
 """
 from PyQt4.QtCore import QSettings, QTranslator, qVersion, QCoreApplication, Qt
 from PyQt4.QtGui import QAction, QIcon
@@ -210,7 +201,7 @@ class GeodataNLUI:
     #--------------------------------------------------------------------------
 
     def run(self):
-        
+
         """Run method that loads and starts the plugin"""
         if not self.pluginIsActive:
             self.pluginIsActive = True
