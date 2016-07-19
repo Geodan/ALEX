10ba64049fb324273018b380740844928cbb60c9
Alexander Freeman <alexander.freeman.nl@gmail.com>
2016-07-19 10:09:16 +0200

    Added the empty function calls in the sequelizer.handle_request method

--- QueryBuilder/Sequelizer.py
+++ QueryBuilder/Sequelizer.py
@@ -27,11 +27,34 @@ class Sequelizer(object):
                 sqlf=self.to_sql_and_run,
                 geojf=self.convert_to_geojson
                 ):
-        self.cf = cf # Classification Function
-        self.dsf = dsf # Dataset Identification Function
-        self.lbf = lbf # Logical Bindings Function
-        self.sqlf = sqlf # SQL Function
-        self.geojf = geojf # GeoJSON unction
+        self.fn_classify = cf
+        self.fn_identify_dataset = dsf
+        self.fn_logical_bindings = lbf
+        self.fn_to_sql_and_run = sqlf
+        self.fn_convert_to_geojson = geojf
 
     def handle_request(self, sentence):
-        pass
+        if type(sentence) != str:
+            raise ValueError("Sentence is not a string")
+
+        language_objects = self.fn_classify(sentence)
+
+        # TODO check nlp result
+
+        semi_query = self.fn_identify_dataset(language_objects)
+
+        # TODO check dataset result
+
+        logical_sentence = self.fn_logical_bindings(semi_query)
+
+        # TODO check logical bindings
+
+        sql_results = self.fn_to_sql_and_run(logical_sentence)
+
+        # TODO Check SQL results
+
+        geojson = self.fn_convert_to_geojson(sql_results):
+
+        # TODO Check geojson result
+
+        return geojson
