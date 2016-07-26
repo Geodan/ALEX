fdb9e9ea4d35068f8f334763db60832d9328d30a
Alexander Freeman <alexander.freeman.nl@gmail.com>
2016-07-26 10:30:43 +0200

    Removed commands.sequelize

--- QueryBuilder/Commands.py
+++ QueryBuilder/Commands.py
@@ -5,26 +5,3 @@ class Command(WordGroup):
 
     def __init__(self, words):
         super().__init__(words)
-
-    def sequelize(self, arguments, context):
-        if len(arguments) <= 0:
-            return {"error_code": 1, "error_messages": "No arguments?"}
-        if type(arguments[0]) != Arguments.SearchQuery:
-            raise MalformedSentenceException("No SearchQuery to find")
-
-        if not "databases" in context:
-            context["databases"] = []
-
-        sql_statements = []
-        while type(arguments[0]) == Arguments.SearchQuery:
-            arg = arguments.pop(0)
-            context["databases"].append(arg.get_database())
-            temp = arg.search.replace (" ", "_")
-            sql_statements.append(("AND (building LIKE '" + temp + "'", 1000))
-            sql_statements.append(("OR amenity LIKE '" + temp + "')", 1001))
-
-        sql_statements.append(("SELECT ST_AsGeoJSON(way) FROM {databases} WHERE ", 0))
-        return sql_statements
-
-    def __str__(self):
-        return "Command: " + " ".join(self.words)
