ac8e9b8fd25df83c7819406f07c3a8287b2142d3
Alexander Freeman <alexander.freeman.nl@gmail.com>
2016-07-26 09:07:16 +0200

    Added LogicClassifier en BindingClassifier

--- QueryBuilder/Classifiers.py
+++ QueryBuilder/Classifiers.py
@@ -1,9 +1,21 @@
 import Filters
 
-class HardCodedFilterClassifier:
+import Logic
 
-    def __init__(self):
-        pass
+class HardCodedLogicClassifier:
+
+    def classify(self, words, sentence, current_index, context=None):
+
+        invert = ["no"]
+        reference = ["of", "from"]
+
+        if words in radius:
+            return Filters.RadiusFilter(words)
+        if words in reference:
+            return Filters.ReferenceFilter(words)
+
+
+class HardCodedFilterClassifier:
 
     def classify(self, words, sentence, current_index, context=None):
 
@@ -14,3 +26,15 @@ class HardCodedFilterClassifier:
             return Filters.RadiusFilter(words)
         if words in reference:
             return Filters.ReferenceFilter(words)
+
+class HardCodedBindingClassifier:
+
+    def classify(self, words, sentence, current_index, context=None):
+
+        existence = ["where there is", "where there are"]
+        combine = ["and"]
+
+        if words in existence:
+            return Logic.ExistenceBinding(words)
+        if words in combine:
+            return None #Must be combinding :')
