f7aad5fad391929de49204fa1d23b5799471409a
Alexander Freeman <alexander.freeman.nl@gmail.com>
2016-07-27 14:25:56 +0200

    Added first SQLization of Dataset

--- QueryBuilder/Datasets.py
+++ QueryBuilder/Datasets.py
@@ -1,9 +1,16 @@
 from enum import Enum
+import Subsets
 
 # Maybe useful later on
 class ConnectionTypes(Enum):
     SQL = 1
     HTTP = 2
+
+class DatasetCombiner(object):
+
+    def __init__(self, datasets):
+        pass
+
 
 def StatisticalModel(object):
 
@@ -12,16 +19,20 @@ def StatisticalModel(object):
 
 class GeoDataset(object):
 
-    def __init__(self, db_name, content, table, columns=None):
+    def __init__(self, db_name, conn, content, table, columns=None):
+        self.conn = conn
         self.db_name = db_name.lower()
         self.content = content
         self.table = table
 
-        if columns == None:
+        if not columns:
             # TODO Get the columns
             self.columns = []
         else:
             self.columns = columns
+
+    def get_subset(self, subset):
+        raise NotImplementedError
 
     def map_keyword_to_tags(self, word):
         raise NotImplementedError
@@ -33,8 +44,46 @@ class GeoDataset(object):
 
 class OSMTable(GeoDataset):
 
-    def __init__(self, content, table, columns):
-        super().__init__("OSM", content, table, columns)
+    def __init__(self, conn, content, table, columns):
+        super().__init__("OSM", conn, content, table, columns)
+
+    def get_subset(self, subset):
+        subset_type = type(subset)
+        sql = ""
+        if subset.relative:
+            return ""
+        if subset_type == Subsets.RadiusSubset:
+            sql += """
+            WITH %s AS (
+                SELECT ST_Buffer(ST_Transform(ST_SetSRID(ST_MakePoint(%s, %s), %s), 3857), %s) geom
+            )
+            """ % (
+                subset.id + "_radius",
+                subset.location[0],
+                subset.location[1],
+                subset.location[2],
+                subset.distance.value
+            )
+
+            sql += """
+            WITH %s AS (
+                SELECT %s.way, %s, %s
+                WHERE way IS NOT NULL AND
+                    NOT ST_IsEmpty(way) AND
+                    ST_Intersects(way, %s.geom)
+                    AND %s LIKE '%s';
+            )
+            """ % (
+                subset.id,
+                self.table,
+                self.table,
+                subset.id + "_radius",
+                subset.id + "_radius",
+                self.map_keyword_to_tags(subset.search_query.search)[0],
+                self.map_keyword_to_tags(subset.search_query.search)[1]
+            )
+
+        return sql
 
     def get_location_sql(self):
         return "ST_Centroid(way)"
@@ -42,9 +91,10 @@ class OSMTable(GeoDataset):
 
 class OSMPolygonTable(OSMTable):
 
-    def __init__(self):
+    def __init__(self, conn):
         super().__init__(
             "lines",
+            conn,
             "planet_osm_polygon",
             [
                 "building",
@@ -57,13 +107,14 @@ class OSMPolygonTable(OSMTable):
         )
 
     def map_keyword_to_tags(self, word):
-        return ()
+        return (("building", word))
 
 class OSMLinesTable(OSMTable):
 
-    def __init__(self):
+    def __init__(self, conn):
         super().__init__(
             "areas",
+            conn,
             "planet_osm_line",
             [
                 "railway",
@@ -76,9 +127,11 @@ class OSMLinesTable(OSMTable):
         )
 
     def map_keyword_to_tags(self, word):
+        if word in ["road"]:
+            return [("highway", "*")]
         if word in ["train_rail", "train_line", "trainrail", "trainline"]:
             return [("railway", "rail")]
-        if word in ["railroads", "rail", "railway"]:
+        if word in ["railroad", "rail", "railway"]:
             return [("railway", "rail"), ("railway", "subway")]
         if word in ["subway", "subway_rail","subway_line"]:
             return [("railway", "subway")]
