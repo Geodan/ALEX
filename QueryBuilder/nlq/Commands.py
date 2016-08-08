2c3a215df4672cada41c718c0e02fa5f4422c7af
Alexander Freeman <alexander.freeman.nl@gmail.com>
2016-08-08 15:01:11 +0200

    Commands.py PEP8 compliant

--- QueryBuilder/nlq/Commands.py
+++ QueryBuilder/nlq/Commands.py
@@ -1,7 +1,10 @@
 from . import Arguments
 from .WordGroup import WordGroup
 
-class Command(WordGroup):
 
+class Command(WordGroup):
+    """
+    For now an empty class that serves as a type identifier
+    """
     def __init__(self, words):
         super().__init__(words)
